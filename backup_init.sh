f5faa38f0e7ddfc8d8add851e0ab3eaaf27878ce
Rob Kiefer <rob.kiefer@gmail.com>
2017-11-02 11:50:34 -0400

    Make wal-e backup setup more robust, particulary for recovery

--- backup_init.sh
+++ backup_init.sh
@@ -3,14 +3,31 @@ set -e
 
 : "${PGDATA:?PGDATA must be set}"
 
+WGET_BIN=${WGET_BIN:-/usr/bin/wget}
 USE_WALE_SIDECAR=${USE_WALE_SIDECAR:-false}
 WALE_INIT_LOCKFILE=$PGDATA/wale_init_lockfile
 WALE_SIDECAR_HOSTNAME=${WALE_SIDECAR_HOSTNAME:-localhost}
 WALE_SIDECAR_PORT=${WALE_SIDECAR_PORT:-5000}
-ARCHIVE_COMMAND="archive_command='/usr/bin/wget "${WALE_SIDECAR_HOSTNAME}":"${WALE_SIDECAR_PORT}"/push/%f -O -'"
+ARCHIVE_COMMAND="archive_command='"${WGET_BIN}" "${WALE_SIDECAR_HOSTNAME}":"${WALE_SIDECAR_PORT}"/push/%f -O -'"
 
 if [ $USE_WALE_SIDECAR = 'true' ]; then
-    touch $WALE_INIT_LOCKFILE
+    set +e
+    ${WGET_BIN} ${WALE_SIDECAR_HOSTNAME}:${WALE_SIDECAR_PORT}/ping
+    if [ $? -ne 0 ] ; then
+        echo "Create wal-e lock file"
+        touch $WALE_INIT_LOCKFILE
+    fi
+    set -e
+
+    if [ -f $PGDATA/postmaster.pid ] ; then
+        set +e
+        pg_isready
+        if [ $? -ne 0 ] ; then
+            echo "Removing leftover postmaster file"
+            rm $PGDATA/postmaster.pid
+        fi
+        set -e
+    fi
 
     if [ ! -f $PGDATA/postgresql.conf ] ; then
         echo $ARCHIVE_COMMAND >> /usr/local/share/postgresql/postgresql.conf.sample
@@ -26,4 +43,6 @@ else
    echo "Running without wal-e sidecar"
 fi
 
-./docker-entrypoint.sh $@
+echo "Launching postgres"
+echo "$@"
+./docker-entrypoint.sh "$@"
