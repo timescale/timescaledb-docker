afe9bc2fcf2497ec8ce3cba647f3f9a07d931daa
Rob Kiefer <rob.kiefer@gmail.com>
2018-09-12 08:13:31 -0400

    Release 1.0.0-rc1, restore adding extension to template1

--- docker-entrypoint-initdb.d/install_timescaledb.sh
+++ docker-entrypoint-initdb.d/install_timescaledb.sh
@@ -2,7 +2,7 @@
 
 # create extension timescaledb in initial databases
 psql -U postgres postgres -c "CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;"
-#psql -U postgres template1 -c "CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;"
+psql -U postgres template1 -c "CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;"
 
 if [ "$POSTGRES_DB" != 'postgres' ]; then
   psql -U postgres ${POSTGRES_DB} -c "CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;"
