f4b81210659239e6a5aa7d78f565fbbc16bf0808
Sven Klemm <sven@timescale.com>
2018-09-20 05:56:42 +0200

    Use POSTGRES_USER instead of hardcoded user name

--- docker-entrypoint-initdb.d/install_timescaledb.sh
+++ docker-entrypoint-initdb.d/install_timescaledb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # create extension timescaledb in initial databases
-psql -U postgres postgres -c "CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;"
-psql -U postgres template1 -c "CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;"
+psql -U "${POSTGRES_USER}" postgres -c "CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;"
+psql -U "${POSTGRES_USER}" template1 -c "CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;"
 
 if [ "$POSTGRES_DB" != 'postgres' ]; then
-  psql -U postgres ${POSTGRES_DB} -c "CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;"
+  psql -U "${POSTGRES_USER}" "${POSTGRES_DB}" -c "CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;"
 fi
