93602ca157152f22bbbcf35216d5dadd6ad86e08
Sven Klemm <sven@timescale.com>
2018-09-20 06:21:14 +0200

    Use PGDATA to find pg_hba.conf

--- docker-entrypoint-initdb.d/reenable_auth.sh
+++ docker-entrypoint-initdb.d/reenable_auth.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # reenable password authentication
-sed -i "s/host all all all trust/host all all all md5/" /var/lib/postgresql/data/pg_hba.conf
+sed -i "s/host all all all trust/host all all all md5/" "${PGDATA}/pg_hba.conf"
