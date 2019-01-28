6039202dafcc6b5b619662134ad4851f3316749f
Rob Kiefer <rob.kiefer@gmail.com>
2019-01-28 11:44:41 -0500

    Refactor Dockerfiles to reduce build times
    Building the old versions in a separate stage of the Dockerfile
    allows us to reuse those versions in later releases. This will
    reduce build times so releases can get out quicker, especially
    when we add more dimensions (e.g., open-source only images).
    
    Additionally, now the bitnami image and base image share the same
    entry-point dirs so that there is less code duplication.

--- bitnami/docker-entrypoint-initdb.d/001_reenable_auth.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# reenable password authentication
-sed -i "s/host all all all trust/host all all all md5/" "/bitnami/postgresql/data/pg_hba.conf"
--- docker-entrypoint-initdb.d/001_reenable_auth.sh
+++ docker-entrypoint-initdb.d/001_reenable_auth.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+if [ -z "${PGDATA}" ] && [ ! -z "${BITNAMI_IMAGE_VERSION}" ]; then
+	PGDATA=${POSTGRESQL_DATA_DIR}
+fi
+
 # reenable password authentication
 sed -i "s/host all all all trust/host all all all md5/" "${PGDATA}/pg_hba.conf"
