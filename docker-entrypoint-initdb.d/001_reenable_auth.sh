9cf310e8d77ac2247e908fc83ccc2b1378d72942
Sven Klemm <sven@timescale.com>
2020-10-21 00:02:46 +0200

    Remove reeneable auth script
    Since postgres docker image now allow authentication with environment
    variables this script is no longer needed.

--- docker-entrypoint-initdb.d/001_reenable_auth.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -z "${POSTGRESQL_CONF_DIR:-}" ]; then
-        POSTGRESQL_CONF_DIR=${PGDATA}
-fi
-
-# reenable password authentication
-sed -i "s/host all all all trust/host all all all md5/" "${POSTGRESQL_CONF_DIR}/pg_hba.conf"
